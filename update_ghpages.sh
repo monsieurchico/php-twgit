c714ebc14586d2e99e58b96205ac638ac45400dd
Romain Derocle <rderocle@prismamedia.com>
2016-01-27 14:40:27 +0100

    add update_ghpages.sh

---
+++ update_ghpages.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+git checkout master
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+git pull --rebase
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+cp -R deploy /tmp 
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+git checkout gh-pages 
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+git pull --rebase 
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+cp -R /tmp/deploy . 
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+git commit -am "$(cat ./deploy/REVISION.md)" 
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+git push origin HEAD
+if [ $? -ne 0 ]; then
+    exit -1;
+fi
+git checkout master
